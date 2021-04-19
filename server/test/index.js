21a62abec582b7b0dfa6073efd1443fcd4661601
Didoshotev <didoshotev@gmail.com>
2021-04-19 20:57:35 +0300

    feat: back-end
    Added backend and initial setup
    GraphQL
    Mongoose
    Successful log in on front-end
    Setting cookie after log in

---
+++ server/test/index.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const { ApolloServer } = require('apollo-server-express');
+const cors = require('cors');
+const express = require('express');
+const expressJwt = require('express-jwt');
+const jwt = require('jsonwebtoken');
+const http = require('http')
+const cookieParser = require('cookie-parser');
+const config = require('./config');
+const dbConnection = require('./config/database');
+
+const jwtSecret = Buffer.from('xkMBdsE+P6242Z2dPV3RD91BPbLIko7t', 'base64');
+
+const app = express()
+app.use(cors({
+    exposedHeaders: 'Authorization',
+}), express.json(), expressJwt({
+    credentialsRequired: false,
+    secret: jwtSecret,
+    algorithms: ['HS256']
+}),
+    app.use(express.urlencoded({
+        extended: true
+    })),
+    app.use(cookieParser(jwtSecret))
+);
+
+const typeDefs = fs.readFileSync('./schema.graphql', { encoding: 'utf8' });
+const resolvers = require('./resolvers');
+
+
+function context({ req, connection }) {
+    if (req && req.user) {
+        return { userId: req.user.sub };
+    }
+    if (connection && connection.context && connection.context.accessToken) {
+        const decodedToken = jwt.verify(connection.context.accessToken, jwtSecret)
+        return { userId: decodedToken.sub }
+    }
+    return {};
+}
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers, context });
+apolloServer.applyMiddleware({ app, path: '/graphql' });
+
+app.post('/login', (req, res) => {
+    const { name, password } = req.body;
+    const user = db.users.get(name);
+    if (!(user && user.password === password)) {
+        res.sendStatus(401);
+        return;
+    }
+    const token = jwt.sign({ sub: user.id }, jwtSecret);
+    res.send({ token });
+});
+
+// app.listen(port, () => console.log(`Server started on port ${port}`));
+const httpServer = http.createServer(app);
+apolloServer.installSubscriptionHandlers(httpServer)
+httpServer.listen(config.port, () => console.log(`Server started on port ${config.port}`));
---
+++ src/graphql/client.js
@@ -0,0 +1,49 @@
+import {
+    ApolloClient, ApolloLink, HttpLink, InMemoryCache, split
+  } from 'apollo-boost';
+  import { WebSocketLink } from 'apollo-link-ws'
+  import { getMainDefinition } from 'apollo-utilities'
+//   import { getAccessToken } from '../auth';
+  
+  const httpUrl = 'http://localhost:9000/graphql';
+  const wsUrl = 'ws://localhost:9000/graphql';
+  
+  
+  const httpLink = ApolloLink.from([
+    new ApolloLink((operation, forward) => {
+    //   const token = getAccessToken();
+    //   if (token) {
+    //     operation.setContext({headers: {'authorization': `Bearer ${token}`}});
+    //   }
+      return forward(operation);
+    }),
+    new HttpLink({uri: httpUrl})
+  ]);
+  
+  const wsLink = new WebSocketLink({
+    uri: wsUrl,
+    options: {
+    //   lazy: true,
+      reconnect: true,
+    //   connectionParams: () => ({
+    //     accessToken: getAccessToken()
+    //   })
+    }
+  })
+  
+  const isSubsciption = (operation) => {
+    // console.log('isSubscription: ', operation);
+    const definition = getMainDefinition(operation.query)
+    // console.log(definition);
+    return definition.kind === 'OperationDefinition'
+    && definition.operation === 'subscription'
+  }
+  
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: split(isSubsciption, wsLink, httpLink),
+    defaultOptions: {query: {fetchPolicy: 'no-cache'}}
+  });
+  
+  export default client;
+  
